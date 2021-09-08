259b1674ec8e40ff2f8ec7b3e33fbb4888b4a0f6
ABHyeon <38369134+ABHyeon@users.noreply.github.com>
2021-09-08 13:26:04 +0900

    Create calculation.sol

---
+++ calculation.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.4.11;
+
+contract Example005 {
+  function operation() returns (uint result) {
+    result = add(11,5);
+  }
+    
+    function add(uint a, uint b) returns (uint) {
+      uint result = a - b;
+      return result;
+    }
+    
+    function sub(uint a, uint b) returns (uint) {
+      uint result = a - b;
+      return result;
+    }
+    
+    function mul(uint a, uint b) returns (uint) {
+      uint result = a * b;
+      return result;
+    }
+    
+    function div(uint a, uint b) returns (uint) {
+      uint result = a / b;
+      return result;
+    }
+}
